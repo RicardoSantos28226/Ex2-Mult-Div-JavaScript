fa62393d6bab880f9a11048d9d749309f190b26f
RicardoSantos28226 <113592610+RicardoSantos28226@users.noreply.github.com>
2022-09-19 11:46:38 +0100

    Add files via upload

---
+++ Multiplicacao_e_divisao_Ex2/main.js
@@ -0,0 +1,19 @@
+function multiplicaBy()
+    {
+      let num1 = document.getElementById(
+        "N1").value;
+      let num2 = document.getElementById(
+        "N2").value;
+      document.getElementById(
+        "Resultado").innerHTML = num1 * num2;
+    }
+
+    function divideBy() 
+    { 
+      let num1 = document.getElementById(
+        "N1").value;
+      let num2 = document.getElementById(
+        "N2").value;
+      document.getElementById(
+        "Resultado").innerHTML = num1 / num2;
+    }
